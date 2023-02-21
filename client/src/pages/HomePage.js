cc610b10a1f51f90a1c7df14f73cf5b6f40933e3
kimgihyeok <gh9812301@naver.com>
2023-02-21 15:54:07 +0900

    getnewData

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { newsAction } from '../redux/aciton/newsAction'
 const HomePage = () => {
-  const {user} = useSelector((state)=> state.userInfo)
-  console.log(" dbwudw",user)
+
+  const [category,setCategory] = useState("sport")
+  const dispatch = useDispatch();
+  const {newsData} = useSelector((state)=> state.news)
+  useEffect(()=> {
+    dispatch(newsAction.getNews(category))
+  },[])
+
+  console.log("뉴스데이타~",newsData)
+
   return (
     <div>HomePage</div>
   )
 }
 
 export default HomePage
+
+
+
+
+//메인은 사진,타이틀 정도
+// 나머지 기사는 크롤링을 해서 가져오자.
--- client/src/redux/aciton/newsAction.js
+++ client/src/redux/aciton/newsAction.js
@@ -1,7 +1,19 @@
-function getNews() {
-    return(dispatch) =>{
-        
-        let newsData = {}
+
+import axios from "axios";
+import { useState } from "react";
+function getNews (category) {
+    let newsData = {}
+    return async(dispatch) =>{
+        try {
+            const response = await axios.get(
+              `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=95dbe05fd0a94565ad2bb9718feb8d06`,
+            );
+            newsData = response.data
+            console.log(response.data)
+          } catch(e) {
+            console.log(e);
+          }    
+       
         console.log("안녕")
         dispatch({
             type: "SUCCESS_GET_NEWS",
